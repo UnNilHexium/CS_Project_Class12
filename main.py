7cdbae662fd1b843e6874199ef2b630f95c4fc47
UnNilHexium <11199@sanskritischool.edu.in>
2026-09-24 05:29:29 +0000

    card generation online!

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-#   Project by UnNilHexium, TepicKid, WhiteDiger
-#                UnNilHexium - Codebase, Debugging
-#                TepicKid    - Idea, Synopsis
-#                WhiteDiger  - Testing, Pitch
-#                Gemini AI   - Teaching/Guidance/Code
-
 import mysql.connector as m
 from PIL import Image, ImageDraw, ImageFont
+import textwrap
 
 try:
     gamer_data = m.connect(host="localhost", user="root", password="")
@@ -24,7 +19,7 @@ except Exception:
     print("DB does not exist. Creating database...")
     db_cur.execute("CREATE DATABASE gamer_data")
     db_cur.execute("USE gamer_data")
-  
+
 def Create_Table():
     table = '''CREATE TABLE IF NOT EXISTS general_info (
         gamer_tag VARCHAR(20) PRIMARY KEY,
@@ -42,8 +37,6 @@ def Create_Table():
         Social_Other_UName VARCHAR(50)
     )'''
     db_cur.execute(table)
-
-
 
 def Commit_to_Table(data):
     try:
@@ -91,6 +84,29 @@ def Fetch_Game_Tag_Data():
         return 1
     return result
 
+def draw_mixed_text(draw_obj, pos, text, font_main, font_num, fill):
+    x, y = pos
+    text_str = str(text)
+    if not text_str:
+        return
+    current_chunk = ""
+    is_num_chunk = text_str[0].isdigit()
+    for char in text_str:
+        char_is_num = char.isdigit()
+        if char_is_num == is_num_chunk:
+            current_chunk += char
+        else:
+            font = font_num if is_num_chunk else font_main
+            draw_obj.text((x, y), current_chunk, fill=fill, font=font)
+            bbox = font.getbbox(current_chunk)
+            if bbox:
+                x += (bbox[2] - bbox[0])
+            current_chunk = char
+            is_num_chunk = char_is_num
+    if current_chunk:
+        font = font_num if is_num_chunk else font_main
+        draw_obj.text((x, y), current_chunk, fill=fill, font=font)
+
 def Gen_Card(): 
     data = Fetch_Game_Tag_Data()
     if data == 1:
@@ -111,8 +127,68 @@ def Gen_Card():
         
         draw_obj.line([(0, i), (1920, i)], fill=(r, g, b))
     
-    f_title=ImageFont.truetype("")
+    f_title=ImageFont.truetype("/workspaces/CS_Project_Class12/fonts/titlefont.ttf", 60)
+    f_body_alt=ImageFont.truetype("/workspaces/CS_Project_Class12/fonts/bodyfont.ttf", 36)
+    f_body=ImageFont.truetype("/workspaces/CS_Project_Class12/fonts/bodyalt.ttf",36)
+    f_num=ImageFont.truetype("/workspaces/CS_Project_Class12/fonts/numberfont.ttf",36)
+    t_color = (0,0,0)
 
+    title_text = "GAMER CARD: %s" % gamer_tag.upper()
+    
+    bbox = f_title.getbbox(title_text)
+    text_width = bbox[2] - bbox[0]
+    title_x = (1920 - text_width) // 2
+    title_y = 60
+    
+    draw_obj.text((title_x, title_y), title_text, fill=t_color, font=f_title)
+
+    draw_obj.line([(150, 150), (1770, 150)], fill=t_color, width=4)
+
+    col1_x = 150
+    col1_y = 200
+
+    draw_obj.text((col1_x, col1_y), "[ PLAYER STATS & GAMES ]", fill=t_color, font=f_body_alt)
+    col1_y += 60
+
+    stats_list = [
+        "Age: %s" % age,
+        "Gaming EXP: %s years" % exp,
+        "Best Game: %s" % g_perf,
+        "Favorite Game: %s" % g_like,
+        "Secondary Game: %s" % g_sec,
+        "Best Story: %s" % g_story,
+        "Best Graphics: %s" % g_graph
+    ]
+
+    for item in stats_list:
+        wrapped_lines = textwrap.wrap(item, width=32)
+        for line in wrapped_lines:
+            draw_mixed_text(draw_obj, (col1_x, col1_y), line, f_body, f_num, t_color)
+            col1_y += 42
+        col1_y += 12  
+
+    col2_x = 1050
+    col2_y = 200
+
+    draw_obj.text((col2_x, col2_y), "[ SOCIAL PROFILES ]", fill=t_color, font=f_body_alt)
+    col2_y += 60
+
+    socials_list = [
+        "Discord: %s" % discord,
+        "Steam: %s" % steam,
+        "YouTube: %s" % youtube
+    ]
+    
+    if o_plat and o_uname:
+        extra_social = "%s: %s" % (o_plat, o_uname)
+        socials_list.append(extra_social)
+
+    for item in socials_list:
+        wrapped_lines = textwrap.wrap(item, width=28)
+        for line in wrapped_lines:
+            draw_mixed_text(draw_obj, (col2_x, col2_y), line, f_body, f_num, t_color)
+            col2_y += 42
+        col2_y += 12
 
     name = "%s Card.png" % data[0]
     image.save(name)
@@ -122,5 +198,8 @@ def Bring_gamer_data():
     db_cur.execute("Select * from general_info")
     print(db_cur.fetchall())
 
-
-Gen_Card()
+print("Welcome to CardGen.Co, where you can make yourself a Business Card, But for gamers.")
+Print('''You can -
+1. Add an entry of your username to the database(A)
+2. Inspect all entries(I)
+3. Generate your very own Gamer Card(G) ''')
